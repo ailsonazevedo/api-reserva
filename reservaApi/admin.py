71add898a8c3b804830b535186f1269e2a4b789b
ailsonazevedo <ailsonsilvaazevedo1@gmail.com>
2022-03-16 10:53:21 -0300

    create models

--- reservaApi/admin.py
+++ reservaApi/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Key)
+class KeyAdmin(admin.ModelAdmin):
+    list_display = ['numero','nome']
+    list_filter = ['nome','numero']
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ['key','first_name','date_reservation','date_devoltution','reservation']
+    list_filter = ['date_reservation','key','reservation']
+    search_fields = ['key']
+
+
--- reservaApi/models.py
+++ reservaApi/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import uuid
+
+from django.db.models.fields import DateTimeField
+
+
+
+class Key (models.Model):
+    nome = models.CharField('Nome da chave',max_length=150)
+    numero = models.DecimalField('Número da chave', max_digits=5,decimal_places=0,null=False, blank=False)
+
+    def __str__(self):
+        return str(self.numero)
+
+    class Meta:
+        ordering = ['numero']
+        verbose_name = 'Chave'
+        verbose_name_plural = 'Chaves'
+
+class Reservation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    reservation = models.BooleanField('Reservado?',default=True)
+    first_name = models.CharField('Primeiro Nome',max_length=100)
+    last_name = models.CharField('Ultimo Nome',max_length=100)
+    date_reservation = DateTimeField('Data da Reserva',auto_now_add=True,null=False)
+    date_devoltution = DateTimeField('Data da Devolução',auto_now=False,null=True, blank=True)
+    key = models.ForeignKey(Key,on_delete=models.CASCADE,null=False)
+
+    class Meta:
+        ordering = ['date_reservation']
+        verbose_name = 'Reserva'
+        verbose_name_plural = 'Reservas'
 
 # Create your models here.
